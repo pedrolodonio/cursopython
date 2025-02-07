16f193393703d80b8ee95dc9dfbe5e02876ad04a
Pedro <pedrolodonio20@gmail.com>
2025-02-07 01:16:17 -0300

    adicionando primeiro exercicio

---
+++ exercício1.py
@@ -0,0 +1,16 @@
+#solicitar dois números exibir soma subtração divisão e multiplicação
+
+n1 = float(input("digite o primeiro número:"))
+n2 = float(input("digite agora um outro número:"))
+
+#números digitados
+print("você digitou ",n1)
+print("você digitou ",n2)
+#Soma
+print (n1,"+",n2,"=",(n1+n2))
+#Subtração
+print (n1,"-",n2,"=",(n1-n2))
+#Multiplicação
+print (n1,"*",n2,"=",(n1*n2))
+#Divisão
+print (n1,"/",n2,"=",(n1/n2))
